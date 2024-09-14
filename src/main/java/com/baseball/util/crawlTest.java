0e27245ece4f991ead96d00faddcd543dcbb9afb
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-09-15 01:56:41 +0900

    크롤링 된다.. 팀 이름만 넣어주면 다 뽑을 수 있다.

--- src/main/java/com/baseball/util/crawlTest.java
+++ src/main/java/com/baseball/util/crawlTest.java
@@ -1,20 +1,54 @@
 package com.baseball.util;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
 public class crawlTest {
+
 	public static void crawl() {
 		// 웹드라이버 실행 파일 경로 설정
 		System.setProperty("webdriver.chrome.driver",
@@ -28,10 +62,11 @@ public class crawlTest {
 			// 크롤링할 웹 페이지 URL
 			driver.get("https://www.koreabaseball.com/Player/Search.aspx");
 
-			// 삼성 팀 선택
+			// 팀 드롭다운 메뉴 선택
 			WebElement teamDropdown = wait.until(
 					ExpectedConditions.elementToBeClickable(By.id("cphContents_cphContents_cphContents_ddlTeam")));
 			teamDropdown.click();
+			// 해당 구단 선택
 			WebElement samsungOption = wait
 					.until(ExpectedConditions.elementToBeClickable(By.xpath("//option[text()='삼성']")));
 			samsungOption.click();
@@ -44,17 +79,21 @@ public class crawlTest {
 			extractPlayerData(driver, wait);
 
 			// 페이징
+			int currentPage = 1;
 			while (true) {
-				WebElement nextPageButton = driver.findElement(By.cssSelector(".paging a[title='다음 페이지로 이동']"));
+				WebElement nextPageButton = driver.findElement(
+						By.cssSelector(".paging a#cphContents_cphContents_cphContents_ucPager_btnNo" + currentPage));
+
 				if (nextPageButton.isEnabled()) {
 					nextPageButton.click();
-					wait.until(ExpectedConditions.stalenessOf(driver.findElement(By.cssSelector(".tEx"))));
+					wait.until(ExpectedConditions.stalenessOf(driver.findElement(By.cssSelector("table.tEx"))));
 					extractPlayerData(driver, wait);
 				} else {
 					break; // 더 이상 페이지가 없는 경우 루프 종료
 				}
-			}
 
+				currentPage++;
+			}
 		} finally {
 			// 웹 드라이버 종료
 			driver.quit();
