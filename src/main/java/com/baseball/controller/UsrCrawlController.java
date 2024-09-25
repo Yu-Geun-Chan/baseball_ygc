fb3d323ccac50bd11cc92b99d37bf1f4b5e723b3
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-09-26 00:23:53 +0900

    경기일정 / 결과 페이지 크롤링(01~12월), JSON 파일로 생성

--- src/main/java/com/baseball/controller/UsrCrawlController.java
+++ src/main/java/com/baseball/controller/UsrCrawlController.java
@@ -1,10 +1,14 @@
 package com.baseball.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.baseball.crawlGameSchedule.GameSchedule;
 import com.baseball.crawlPlayer.Doosan;
 import com.baseball.crawlPlayer.Hanwha;
 import com.baseball.crawlPlayer.Kia;
@@ -19,6 +23,7 @@ import com.baseball.crawlPlayer.Ssg;
 @Controller
 public class UsrCrawlController {
 
+	// 구단별 선수 정보
 	@Autowired
 	private Samsung samsung;
 	@Autowired
@@ -39,9 +44,13 @@ public class UsrCrawlController {
 	private Nc nc;
 	@Autowired
 	private Ssg ssg;
+	
+	// 경기 일정
+	@Autowired
+	private GameSchedule gameSchedule;
 
 	@RequestMapping("/usr/crawlPlayer")
-	public String doCrawl() {
+	public String doCrawlPlayer() {
 
 		kia.crawl();
 		samsung.crawl();
@@ -57,4 +66,15 @@ public class UsrCrawlController {
 		return "/usr/home/main";
 
 	}
-}
+	
+	// 크롤링 할때 localhost:8083/usr/crawlGameSchedule?month='01~12' 해당 월을 넣어서 url에 직접 입력해야 크롤링됨..
+	@GetMapping("/usr/crawlGameSchedule")
+	public ResponseEntity<String> doCrawlGameSchedule(@RequestParam String month) {
+	    if (month.matches("^(0[1-9]|1[0-2])$")) {
+	        String result = gameSchedule.crawl(month);
+	        return ResponseEntity.ok(result); // 200
+	    } else {
+	        return ResponseEntity.badRequest().body("유효하지 않은 월입니다. (01~12 사이의 값)"); // 400
+	    }
+	}
+} 
